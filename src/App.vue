87393d94774f33146e0184d9944a521c9bf6699a
zhangyi94033094 <739188729@qq.com>
2021-02-23 21:55:25 +0800

    123-1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <i class="iconfont icondianzan"></i>
     <router-view />
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
 
   </div>
 </template>
